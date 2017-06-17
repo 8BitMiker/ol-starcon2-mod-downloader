7f210318e31db458a63bbdcb62f88b634f0abd93
Michael Limberger <contact@mikelimberger.com>
2017-06-17 17:56:47 -0400

    First commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Zoomify Media Fixes
+
+An issue was identified that was causing hotspot errors. The **MEDIA=** tag in the .xml was pointing to incorrect places. This code was deisgned to fix that.
---
+++ _GET-THOSE-TASTY-SC2-MODS.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# by Mike Limberger
+# One night I needed some hot Starcon2 action, thus this was born!
+
+# You need cURL and perl on your system in your PATH for this to work (you most likely have this.)
+
+curl -sSL http://wiki.uqm.stack.nl/Star_Control_Music | ruby -077 -nle '$_.scan(%r{(?<=href=")https?://[^"]+\.mod}) { |m| puts m }' | xargs -n1 wget --random-wait
+
+# p.s This code is super britle (Some say the way I like it!)
