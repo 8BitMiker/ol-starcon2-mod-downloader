74a7de2ceb4ac0247a3fc2dcaa1cf80a1a6ec27e
Michael Limberger <contact@mikelimberger.com>
2017-06-17 18:02:21 -0400

    added newlines for readability.

--- _GET-THOSE-TASTY-SC2-MODS.sh
+++ _GET-THOSE-TASTY-SC2-MODS.sh
@@ -5,6 +5,8 @@
 
 # You need cURL and perl on your system in your PATH for this to work (you most likely have this.)
 
-curl -sSL http://wiki.uqm.stack.nl/Star_Control_Music | ruby -077 -nle '$_.scan(%r{(?<=href=")https?://[^"]+\.mod}) { |m| puts m }' | xargs -n1 wget --random-wait
+curl -sSL http://wiki.uqm.stack.nl/Star_Control_Music \
+| ruby -077 -nle '$_.scan(%r{(?<=href=")https?://[^"]+\.mod}) { |m| puts m }' \
+| xargs -n1 wget --random-wait
 
 # p.s This code is super britle (Some say the way I like it!)
